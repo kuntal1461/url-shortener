8865ba78beaff5d33f13429490e050824f54e793
kuntal.1461 <kuntal.1461@gmail.com>
2024-07-15 19:36:26 +0530

    short url

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module url-shortener
 
 go 1.22.4
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/lib/pq v1.10.9
+	github.com/segmentio/ksuid v1.0.4
+)
--- handlers/shorten.go
+++ handlers/shorten.go
@@ -1,6 +1,45 @@
 package handlers
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+	"url-shortener/db"
+	"url-shortener/models"
 
-func main1()  {
+	"github.com/segmentio/ksuid"
+)
+
+// CreateShortURL handles the creation of short URLs
+func CreateShortURL(w http.ResponseWriter , r*http.Request)  {
 	
+	var request struct{
+		OriginalUrl string `json:"original_url"`
+	}
+
+	if err:= json.NewDecoder(r.Body).Decode(&request); err!=nil {
+		http.Error(w,err.Error(),http.StatusBadRequest)
+	}
+
+	id :=ksuid.New().String();
+
+	shortenedURL :=fmt.Sprintf("http://localhost:8080/%s", id)
+
+	url :=models.URL{
+		ID: id,
+		Original: request.OriginalUrl,
+		Shortened:  shortenedURL,
+		Created_at: time.Now(),
+	}
+	
+	_, err := db.GetDB().Exec("INSERT INTO urls (id, original, shortened, created_at) VALUES ($1, $2, $3, $4)", url.ID, url.Original, url.Shortened, url.Created_at)
+
+	if err !=nil {
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+		return
+		
+	}
+	json.NewEncoder(w).Encode(url)
+
 }
