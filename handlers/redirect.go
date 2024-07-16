79ed7c3891a1529f58465c414961d62eba9879da
kuntal.1461 <kuntal.1461@gmail.com>
2024-07-16 12:53:57 +0530

    enable loging

--- handlers/redirect.go
+++ handlers/redirect.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 	"url-shortener/db"
 	"url-shortener/models"
@@ -17,11 +18,14 @@ func RedirectURL(w http.ResponseWriter, r *http.Request)  {
 
 	if err== sql.ErrNoRows{
 		http.NotFound(w,r)
+		log.Println("No URL found for ID:", id)
 		return
 	}else if err!=nil{
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println("Error querying database:", err)
 		return
 	}
+	log.Println("Redirecting to original URL:", url.Original)
 	http.Redirect(w, r, url.Original, http.StatusFound)
 	
 }
--- handlers/shorten.go
+++ handlers/shorten.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 	"url-shortener/db"
@@ -12,34 +13,40 @@ import (
 )
 
 // CreateShortURL handles the creation of short URLs
-func CreateShortURL(w http.ResponseWriter , r*http.Request)  {
-	
-	var request struct{
+func CreateShortURL(w http.ResponseWriter, r *http.Request) {
+
+	var request struct {
 		OriginalUrl string `json:"original_url"`
 	}
 
-	if err:= json.NewDecoder(r.Body).Decode(&request); err!=nil {
-		http.Error(w,err.Error(),http.StatusBadRequest)
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Println("Error decoding request:", err)
+		return
 	}
 
-	id :=ksuid.New().String();
+	id := ksuid.New().String()
 
-	shortenedURL :=fmt.Sprintf("http://localhost:8082/%s", id)
+	shortenedURL := fmt.Sprintf("http://localhost:8082/%s", id)
 
-	url :=models.URL{
-		ID: id,
-		Original: request.OriginalUrl,
+	url := models.URL{
+		ID:         id,
+		Original:   request.OriginalUrl,
 		Shortened:  shortenedURL,
 		Created_at: time.Now(),
 	}
-	
+
 	_, err := db.GetDB().Exec("INSERT INTO urls (id, original, shortened, created_at) VALUES ($1, $2, $3, $4)", url.ID, url.Original, url.Shortened, url.Created_at)
 
-	if err !=nil {
-		http.Error(w,err.Error(),http.StatusInternalServerError)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println("Error inserting URL into the database:", err)
 		return
-		
+
 	}
+	log.Println("Successfully created short URL:", shortenedURL)
+
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(url)
 
 }
