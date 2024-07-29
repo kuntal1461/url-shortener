4a78b1d7d027c2f9ff5c6f365f7ab362d342a18b
kuntal.1461 <kuntal.1461@gmail.com>
2024-07-29 17:08:28 +0530

    redirection issue fix

--- configs/config.go
+++ configs/config.go
@@ -1,1 +1,29 @@
 package configs
+
+import (
+	"encoding/json"
+	"os"
+)
+
+
+
+type Config struct{
+
+	DataBaseDsn string  `json:"databaseDSN"`
+	ServerPort string 	`json:"serverPort"`
+}
+
+func LoadConfig()(*Config, error){
+	configFile,err :=os.Open("../config/config.json")
+	if err !=nil {
+		return nil, err
+	}
+	defer configFile.Close() 
+
+	var config Config
+	jsonParser := json.NewDecoder(configFile)
+	if err=jsonParser.Decode(&config);err !=nil {
+		return nil ,err
+	}
+	return &config ,nil
+}
